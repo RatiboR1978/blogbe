56379cea38339d245313fedc82965594b4f4e653
Ratibor <klov29@mail.ru>
2018-09-23 14:00:16 +0700

    Поправил моб.меню

--- js/common.js
+++ js/common.js
@@ -1,13 +1,30 @@
-var mobileNav = document.querySelector('.mobile-nav'),
-	mobileNavList = document.querySelector('.mobile-nav__list'),
+var mobileNavList = document.querySelector('.mobile-nav__list'),
 	activitiesList = document.querySelectorAll('#activitiesList li a'),
-	arrLinks = [],
-	butActivites = document.querySelector('#activities');
+	countriesList = document.querySelectorAll('#countriesList li a'),
+	citiesList = document.querySelectorAll('#citiesList li a'),
+	buttonsMob = document.querySelectorAll('.mobile-nav button');
 
-butActivites.addEventListener('click', function () {
+buttonsMob.forEach(function (item) {
+	item.addEventListener('click', func);
+});
+
+function func(e) {
 	mobileNavList.innerHTML ='';
-	activitiesList.forEach(function (item) {
+	if (e.target.id === 'activities') {
+		funcClick ('mobile-nav__list-countries', 'mobile-nav__list-cities', 'mobile-nav__list-activities', activitiesList);
+	} else if (e.target.id === 'countries') {
+		funcClick ('mobile-nav__list-activities', 'mobile-nav__list-cities', 'mobile-nav__list-countries',
+			countriesList);
+	} else {
+		funcClick ('mobile-nav__list-activities', 'mobile-nav__list-countries', 'mobile-nav__list-cities', citiesList);
+	}
+}
+
+function funcClick (del1, del2, active, list) {
+	mobileNavList.classList.remove(del1);
+	mobileNavList.classList.remove(del2);
+	mobileNavList.classList.toggle(active);
+	list.forEach(function (item) {
 		mobileNavList.appendChild(item.cloneNode(true));
 	});
-	mobileNavList.classList.toggle("mobile-nav__list--active");
-});
+}
